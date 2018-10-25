e0d5da637753b4234aa6ac3a080633295847212a
ozzzzzzzam <kalanaperera94@gmail.com>
2018-10-25 23:11:54 +0530

    Update QuillEditor.js

--- js/forum/src/components/QuillEditor.js
+++ js/forum/src/components/QuillEditor.js
@@ -44,12 +44,77 @@ export default class QuillEditor extends Component {
 
         this.quill = new Quill('.' + this.props.className, {
             modules: {
-                toolbar: true
+                toolbar: [
+      [{ header: [1, 2, false] }],
+      ['bold', 'italic', 'underline'],
+      ['image', 'code-block']
+    ]
             },
             placeholder: this.props.placeholder || '',
             readOnly: !!this.props.disabled,
             theme: 'snow'
         });
+        
+        
+        
+      /**
+       * Step1. select local image
+       *
+       */
+    function selectLocalImage() {
+      const input = document.createElement('input');
+      input.setAttribute('type', 'file');
+      input.click();
+
+      // Listen upload local image and save to server
+      input.onchange = () => {
+        const file = input.files[0];
+
+        // file type is only image.
+        if (/^image\//.test(file.type)) {
+          saveToServer(file);
+        } else {
+          console.warn('You could only upload images.');
+        }
+      };
+    }
+
+    /**
+     * Step2. save to server
+     *
+     * @param {File} file
+     */
+    function saveToServer(file: File) {
+      const fd = new FormData();
+      fd.append('image', file);
+
+      const xhr = new XMLHttpRequest();
+      xhr.open('POST', 'http://localhost:3000/upload/image', true);
+      xhr.onload = () => {
+        if (xhr.status === 200) {
+          // this is callback data: url
+          const url = JSON.parse(xhr.responseText).data;
+          insertToEditor(url);
+        }
+      };
+      xhr.send(fd);
+    }
+
+    /**
+     * Step3. insert image url to rich editor.
+     *
+     * @param {string} url
+     */
+    function insertToEditor(url: string) {
+      // push image url to rich editor.
+      const range = editor.getSelection();
+      editor.insertEmbed(range.index, 'image', 'http://localhost:9000${url}');
+    }
+
+    // quill editor add image handler
+     this.quill.getModule('toolbar').addHandler('image', () => {
+      selectLocalImage();
+    });
 
         this.quill.clipboard.dangerouslyPasteHTML(0, this.value());
         this.quill.on('text-change', this.oninput.bind(this));
